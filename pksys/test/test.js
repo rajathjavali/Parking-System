de121a6ba47e9c7945c513e3e6380fe3766d4e13
rajathjavali <rajathjavali@gmail.com>
2015-06-04 12:46:42 +0530

    allcha

--- pksys/test/test.js
+++ pksys/test/test.js
@@ -1,4 +1,4 @@
-var expect = require("chai").expect; // node.js core module
+/*var expect = require("chai").expect; // node.js core module
 var c = require('../App/app1.js');
 
 
@@ -34,4 +34,27 @@ it('unpark unSuccessful', function(){
     })
 
   //})
+});*/
+
+
+
+
+var expect = require("chai").expect;
+var parkingManager = require('../App/parkingLot1.js');
+
+describe('Parking System',function(){
+  var parkingLot1 = parkingManager(3);
+  it('Successfull System', function(){
+      
+      
+      var car1 = parkingLot1.park("car1");
+      if(car1 == 1)
+        console.log("car1 parking Successfull");
+      else
+        console.log("car1 parking UnSuccessfull");
+        
+      expect(parkingLot1.park("car2")).to.equal(1); 
+      expect(parkingLot1.park("car3")).to.equal(1); 
+      expect(parkingLot1.park("car4")).to.equal(-1); 
+    })
 });
